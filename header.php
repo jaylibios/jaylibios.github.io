e19848954622f48bd1a0694a2ada606e266db252
Jadelyn Libios <44410354+jaylibios@users.noreply.github.com>
2019-03-14 16:26:16 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Header File</title>
+	<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+	<header id="header" class="flexbox">
+		<div class="container flexbox">
+			<div id="logo" class="container-side">
+				<div class="logo-text">
+					<h1><a href="#"><span class="darkblue">Jadelyn</span> <span class="lightblue">Libios</span></a></h1>
+				</div>
+			</div>
+
+			<div id="navigation" class="container-side">
+				<div class="menu-group flexbox">
+					<nav>
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Projects</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
