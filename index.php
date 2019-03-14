b11f63d0fc827d98615a7d5e07cf77b6a767beab
Jadelyn Libios <44410354+jaylibios@users.noreply.github.com>
2019-03-14 16:25:35 -0500

    add php code header

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World!</title>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script src="js/script.js"></script>
+</head>
+<body>
+	<?php include 'header.php' ?>
+</body>
+</html>
